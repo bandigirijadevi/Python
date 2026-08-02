e6ee40800c97a89737b28dbc3872b5ecfcc47ff7
bandigirijadevi <bandigirijadevi>
2026-08-02 06:27:08 +0530

    Initial Commit

---
+++ list_method.py
@@ -0,0 +1,17 @@
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati'] 
+print(motorcycles) 
+motorcycles.remove('ducati')
+print(motorcycles)
+# remove()  method deletes only the first occurrence of the value you specify
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+cars.sort(reverse=True) 
+print(cars)
+cars.sort(reverse=False)
+print(cars)
+cars.reverse()
+print(cars)
+length = len(cars)
+print(length)
+# methods used is length , remove, sort, reverse
